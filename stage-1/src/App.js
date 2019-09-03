31bed5da6077367ff6153d6a488dd50ad6177bc4
Brian Lowe <mynameisboy@gmail.com>
2019-09-03 14:08:09 -0600

    initial commit

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
